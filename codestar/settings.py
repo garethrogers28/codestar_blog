fe96b99fc2654038e764a0d99004fcfda1071db4
Gareth Rogers <Gareth.Rogers@creditsafeuk.com>
2026-03-17 13:48:17 +0000

    Readied code for deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-imz9gw%x*+do3xfpt-#nhm1cf8*^&-!o^k*d)nuw)1$*4m6sp^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1',
+                 '.herokuapp.com',]
 
 
 # Application definition
